00d3abd05bd661eae8f5427cf78f7aec0a89af75
Minh Tran <t.h.minh101002@gmail.com>
2023-03-29 10:42:03 +0700

    update code

--- src/main/java/com/demo/controller/LoginController.java
+++ src/main/java/com/demo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.demo.controller;
 
+import com.demo.entity.Customer;
+import com.demo.entity.User;
 import com.demo.repository.CustomerRepository;
 import com.demo.repository.UserRepository;
 import com.demo.service.LoginService;
@@ -10,6 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -24,38 +29,27 @@ public class LoginController {
 
     @GetMapping("/loginAccount")
     public ResponseEntity<LoginAPI> checkLogin(@RequestParam("username") String username,
-                                               @RequestParam("password") String password) {
+                                                @RequestParam("password") String password)
+    {
 //        System.out.println(username + " " + password);
         return new ResponseEntity<>(loginService.checkLoginAccount(username, password), HttpStatus.OK);
     }
 
-//    static User user = null;
-//
 //    @GetMapping("/loginGoogle")
-//    public RedirectView currentUser(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
-//        String url = "localhost:3000/welcome";
+//    public ResponseEntity<LoginAPI> currentUser(OAuth2AuthenticationToken oAuth2AuthenticationToken){
 //        String email = oAuth2AuthenticationToken.getPrincipal().getAttributes().get("email").toString();
 //        String name = oAuth2AuthenticationToken.getPrincipal().getAttributes().get("name").toString();
 //        String password = oAuth2AuthenticationToken.getPrincipal().getAttributes().get("at_hash").toString();
 //        System.out.println(email);
 //        System.out.println(name);
 //        System.out.println(password);
-//        user = new User(email, name, password, true, new Date(), email, "0987654321");
+//        User user = new User(email, name, password, true, new Date(), email, "0987654321");
 //        User checkUser = userRepository.findCustomerById(email);
-//        if (checkUser == null) {
+//        if(checkUser == null){
 //            userRepository.save(user);
 //            customerRepository.save(new Customer(email, false, user));
-////             return new ResponseEntity<>(loginService.checkLoginAccount(email, password), HttpStatus.OK);
+//            return new ResponseEntity<>(loginService.checkLoginAccount(email, password), HttpStatus.OK);
 //        }
-////        UrlUtils.isValidRedirectUrl("http://localhost:6969/user/findAll");
-////         return new ResponseEntity<>(loginService.checkLoginAccount(checkUser.getId(), checkUser.getPassword()), HttpStatus.OK);
-////        return new RedirectView(url + "username=" + email + "&password=" + password);
-//        return new RedirectView(url);
+//        return new ResponseEntity<>(loginService.checkLoginAccount(checkUser.getId(), checkUser.getPassword()), HttpStatus.OK);
 //    }
-//
-//    @GetMapping("/loadUser")
-//    public ResponseEntity<LoginAPI> load() {
-//        return new ResponseEntity<>(loginService.checkLoginAccount(user.getId(), user.getPassword()), HttpStatus.OK);
-//    }
-
 }
